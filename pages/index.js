28b0f317e6caf2f9a337eb85417d21a772af1403
Vinicius Catafesta Francisco <vini.ibi@gmail.com>
2019-08-14 14:37:16 -0300

    adaded styles and api service

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import 'isomorphic-fetch';
 
-const Main = () => (
-  <div>
-    <h1>Hello World</h1>
+import './style.css';
+
+const Main = ({ repositories }) => (
+  <div className="container">
+    <h1>Meus repositórios</h1>
+
+    <div>
+      {repositories.map(repo => (
+        <div key={repo.id} className="repo">
+          <h3>{repo.name}</h3>
+          <p>{repo.description}</p>
+          <span>Owner: {repo.owner.login}</span>
+        </div>
+      ))}
+    </div>
   </div>
 );
+
+Main.getInitialProps = async () => {
+  const response = await fetch('https://api.github.com/users/vinicfrancisco/repos');
+  const repositories = await response.json();
+
+  return { repositories };
+};
 
 export default Main;
